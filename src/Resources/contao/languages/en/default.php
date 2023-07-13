ddb372f0af043fee617cb0dc35dc5047521f9b31
Darko Seleši <hallo@w3scouts.com>
2023-07-13 20:18:40 +0200

    Added english translations

---
+++ src/Resources/contao/languages/en/default.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2017 Leo Feyer
+ *
+ *
+ * PHP version 5
+ * @copyright  Martin Kozianka 2011-2017
+ * @author     Martin Kozianka <http://kozianka.de/>
+ * @package    timetags
+ * @license    LGPL
+ * @filesource
+ */
+
+
+$GLOBALS['TL_LANG']['FMD']['timetags_timesince_format']     = "[before %s]:";
+$GLOBALS['TL_LANG']['FMD']['timetags_timesince']            = ["Second", "Minute", "Hour", "Day", "Week", "Month", "Year", "Decade"];
+$GLOBALS['TL_LANG']['FMD']['timetags_timesince_plural']     = ["Seconds", "Minutes", "Hours", "Days", "Weeks", "Month", "Years", "Decades"];
+
+$GLOBALS['TL_LANG']['FMD']['timetags_countdown_plural']     = ["Seconds", "Minutes", "Hours", "Days"];
+$GLOBALS['TL_LANG']['FMD']['timetags_countdown']            = ["Seconds", "Minutes", "Hours", "Days"];
