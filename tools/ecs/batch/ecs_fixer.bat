96a6a1c3cbf43feac65421e7cebb376db8348d40
Darko Seleši <hallo@w3scouts.com>
2024-05-29 20:05:44 +0200

    fix tests

--- tools/ecs/batch/ecs_fixer.bat
+++ tools/ecs/batch/ecs_fixer.bat
@@ -6,8 +6,7 @@ cd..
 cd..
 cd..
 cd..
-php vendor\bin\ecs check vendor/w3scout/contao-thermography-order/src --fix --config vendor/w3scout/contao-thermography-order/tools/ecs/config.php
-php vendor\bin\ecs check vendor/w3scout/contao-thermography-order/contao --fix --config vendor/w3scout/contao-thermography-order/tools/ecs/config.php
-php vendor\bin\ecs check vendor/w3scout/contao-thermography-order/config --fix --config vendor/w3scout/contao-thermography-order/tools/ecs/config.php
-php vendor\bin\ecs check vendor/w3scout/contao-thermography-order/templates --fix --config vendor/w3scout/contao-thermography-order/tools/ecs/config.php
-php vendor\bin\ecs check vendor/w3scout/contao-thermography-order/tests --fix --config vendor/w3scout/contao-thermography-order/tools/ecs/config.php
+php vendor\bin\ecs check vendor/w3scout/contao-timetags-bundle/src --fix --config vendor/w3scout/contao-thermography-order/tools/ecs/config.php
+php vendor\bin\ecs check vendor/w3scout/contao-timetags-bundle/contao --fix --config vendor/w3scout/contao-timetags-bundle/tools/ecs/config.php
+php vendor\bin\ecs check vendor/w3scout/contao-timetags-bundle/config --fix --config vendor/w3scout/contao-timetags-bundle/tools/ecs/config.php
+php vendor\bin\ecs check vendor/w3scout/contao-timetags-bundle/tests --fix --config vendor/w3scout/contao-thermography-order/tools/ecs/config.php
--- tools/ecs/config.php
+++ tools/ecs/config.php
@@ -22,7 +22,7 @@ return static function (ECSConfig $ecsConfig): void {
         ->set(HeaderCommentFixer::class)
         ->call('configure', [
             [
-                'header' => "This file is part of Thermografie.\n\n(c) Darko Selesi ".date('Y')." <hallo@w3scouts.com>\n@license GPL-3.0-or-later\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.\n@link https://github.com/w3scout/contao-thermography-order",
+                'header' => "This file is part of contao-timetags-bundle.\n\n(c) Darko Selesi ".date('Y')." <hallo@w3scouts.com>\n@license GPL-3.0-or-later\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.\n@link https://github.com/w3scout/contao-timetags-bundle",
             ],
         ]);
 
